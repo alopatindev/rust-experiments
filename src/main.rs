dede190fe89d02b33017d3b8e90169770a52729c
Alexander Lopatin <alopatindev@gmail.com>
2016-02-19 16:29:37 +0300

    rename project

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-extern crate algorithms;
+extern crate rust_experiments;
 
 fn array_search() {
     let a: Vec<i32> = vec![10, 22, 55, 66, 66, 333, 1234, 6689];
-    assert_eq!(algorithms::algs::binary_search::bsearch(&a, &55), Some(2));
+    assert_eq!(rust_experiments::algs::binary_search::bsearch(&a, &55), Some(2));
 }
 
 fn print_list() {
-    let x = algorithms::structs::list::Node::new(1, None);
+    let x = rust_experiments::structs::list::Node::new(1, None);
     let y = x.push(2);
     y.print();
     println!("sum={}", y.sum());
