aa12814746252e4d823afe5b84cce4a3ca6b2aa5
Alexander Lopatin <alopatindev@gmail.com>
2016-02-19 16:30:41 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate rust_experiments;
+use rust_experiments::*;
 
 fn array_search() {
     let a: Vec<i32> = vec![10, 22, 55, 66, 66, 333, 1234, 6689];
-    assert_eq!(rust_experiments::algs::binary_search::bsearch(&a, &55), Some(2));
+    assert_eq!(algs::binary_search::bsearch(&a, &55), Some(2));
 }
 
 fn print_list() {
-    let x = rust_experiments::structs::list::Node::new(1, None);
+    let x = structs::list::Node::new(1, None);
     let y = x.push(2);
     y.print();
     println!("sum={}", y.sum());
