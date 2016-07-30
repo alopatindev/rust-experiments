b0a4e6115d5d6a3ffdb36943edeb45d26834b107
Alexander Lopatin <alopatindev@gmail.com>
2016-07-30 12:24:54 +0300

    added to_utf16

--- src/encoding/unicode.rs
+++ src/encoding/unicode.rs
@@ -17,6 +17,34 @@ pub fn to_utf8(utf16: &[u16]) -> Vec<u8> {
     utf8
 }
 
+pub fn to_utf16(utf8: &[u8]) -> Vec<u16> {
+    let mut utf16: Vec<u16> = vec![];
+    let mut i = 0;
+
+    let mut ch;
+    while i < utf8.len() {
+        if (utf8[i] >> 7) == 0 {
+            ch = utf8[i] as u16;
+            i += 1;
+        } else if (utf8[i] >> 5) == 0b110 {
+            let high = ((utf8[i] & 0b00111111) as u16) << 6;
+            let low = (utf8[i + 1] & 0b01111111) as u16;
+            ch = high | low;
+            i += 2;
+        } else {
+            let a = ((utf8[i] & 0b00011111) as u16) << 12;
+            let b = ((utf8[i + 1] & 0b01111111) as u16) << 6;
+            let c = (utf8[i + 2] & 0b01111111) as u16;
+            ch = a | b | c;
+            i += 3;
+        }
+
+        utf16.push(ch);
+    }
+
+    utf16
+}
+
 #[cfg(test)]
 mod tests {
     pub fn make_strings(utf8: &str) -> (Vec<u8>, Vec<u16>) {
@@ -61,6 +89,41 @@ mod tests {
 
             let (utf8, utf16) = make_strings("\u{0800}\u{ffff}");
             assert_eq!(utf8, to_utf8(&utf16[..]));
+        }
+    }
+
+    mod utf8_to_utf16 {
+        use super::*;
+        use super::super::*;
+
+        #[test]
+        fn test_ascii() {
+            let (utf8, utf16) = make_strings("Hello, Rust!");
+            assert_eq!(utf16, to_utf16(&utf8[..]));
+        }
+
+        #[test]
+        fn test_russian() {
+            let (utf8, utf16) = make_strings("Привет, Раст!");
+            assert_eq!(utf16, to_utf16(&utf8[..]));
+        }
+
+        #[test]
+        fn test_japanese() {
+            let (utf8, utf16) = make_strings("こんにちは、さび");
+            assert_eq!(utf16, to_utf16(&utf8[..]));
+        }
+
+        #[test]
+        fn test_bounds() {
+            let (utf8, utf16) = make_strings("\u{0000}\u{007f}");
+            assert_eq!(utf16, to_utf16(&utf8[..]));
+
+            let (utf8, utf16) = make_strings("\u{0080}\u{07ff}");
+            assert_eq!(utf16, to_utf16(&utf8[..]));
+
+            let (utf8, utf16) = make_strings("\u{0800}\u{ffff}");
+            assert_eq!(utf16, to_utf16(&utf8[..]));
         }
     }
 }
