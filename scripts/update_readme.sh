d7121873d0864e279027bff9c6efce409abfad7b
Alexander Lopatin <alopatindev@gmail.com>
2016-08-07 07:55:17 +0300

    started using coveralls.io

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-echo '[![Build Status](https://api.travis-ci.org/alopatindev/rust_experiments.svg?branch=master)](https://travis-ci.org/alopatindev/rust_experiments)'
-echo
-echo '"Hello Worlds" in Rust'
-echo
-
+cat README.md.template
 find src -type f -name '*.rs' | egrep -v '/(lib|mod).rs$' | sed 's!^src/!* !' | sort -u
