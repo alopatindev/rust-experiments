490ccc7f0458a732195e8f29b60971196c72bd45
Alexander Lopatin <alopatindev@gmail.com>
2016-08-22 00:17:38 +0300

    list: rewritten

--- src/structs/list.rs
+++ src/structs/list.rs
@@ -1,21 +1,47 @@
-pub type BoxNode<T> = Box<Node<T>>;
-pub type OptionBoxNode<T> = Option<BoxNode<T>>;
+use std::mem;
 
-pub struct Node<T: PartialEq> {
-    data: T,
-    next: OptionBoxNode<T>,
-}
-
-pub struct List<T: PartialEq> {
-    head: OptionBoxNode<T>,
+pub struct List {
+    head: Link,
     size: usize,
 }
 
-impl<T: PartialEq> List<T> {
-    pub fn new() -> List<T> {
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
         List {
-            head: None,
+            head: Link::Empty,
             size: 0,
+        }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+        self.size += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(boxed_node) => {
+                let node = *boxed_node;
+                self.head = node.next;
+                self.size -= 1;
+                Some(node.elem)
+            }
         }
     }
 
@@ -24,17 +50,22 @@ impl<T: PartialEq> List<T> {
     }
 
     pub fn is_empty(&self) -> bool {
-        self.len() == 0
+        self.size == 0
     }
+}
 
-    pub fn push(&mut self, data: T) {
-        self.size += 1;
-        let tail = self.head.take();
-        let head = Node {
-            data: data,
-            next: tail,
-        };
-        self.head = Some(Box::new(head));
+impl Default for List {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut it = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = it {
+            it = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
     }
 }
 
@@ -44,16 +75,26 @@ mod tests {
 
     #[test]
     fn simple() {
-        let mut xs: List<i32> = List::new();
+        let mut xs = List::new();
+
         assert_eq!(0, xs.len());
         assert!(xs.is_empty());
+        assert_eq!(xs.pop(), None);
 
-        xs.push(1);
-        xs.push(2);
-        xs.push(3);
-        assert!(!xs.is_empty());
-        assert_eq!(3, xs.len());
+        let vec = vec![1, 2, 3];
+        for i in &vec {
+            xs.push(*i);
+        }
 
-        assert_eq!(3, xs.head.unwrap().data);
+        assert_eq!(xs.pop(), Some(3));
+        assert_eq!(xs.pop(), Some(2));
+
+        xs.push(4);
+        xs.push(5);
+
+        assert_eq!(xs.pop(), Some(5));
+        assert_eq!(xs.pop(), Some(4));
+        assert_eq!(xs.pop(), Some(1));
+        assert_eq!(xs.pop(), None);
     }
 }
