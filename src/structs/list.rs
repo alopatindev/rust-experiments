d511343a70d97efd928b6ed4bf39c54ebf1018bb
Alexander Lopatin <alopatindev@gmail.com>
2016-08-23 00:33:02 +0300

    list: implemented skip

--- src/structs/list.rs
+++ src/structs/list.rs
@@ -41,13 +41,36 @@ impl<T> List<T> {
             .map(|rc_node| &rc_node.data)
     }
 
-    // pub fn skip(&self, n: usize) -> Self {
-    //    unimplemented!()
-    // }
-    //
+    pub fn skip(&self, n: usize) -> Self {
+        let mut head = self.head.as_ref();
+        let mut i = 0;
+        while let Some(rc_node) = head {
+            if i < n {
+                head = rc_node.next.as_ref();
+                i += 1;
+            } else {
+                return List { head: Some(rc_node.clone()) };
+            }
+        }
+        List::new()
+    }
+
     // pub fn take(&self, n: usize) -> Self {
     //    unimplemented!()
     // }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(rc_node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(rc_node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -68,11 +91,12 @@ mod tests {
         assert_eq!(Some(&1), xs.tail().tail().head());
         assert_eq!(None, xs.tail().tail().tail().head());
 
-        // assert_eq!(Some(&2), xs.skip(1).head());
-        // assert_eq!(Some(&3), xs.skip(1).tail().head());
-        // assert_eq!(Some(&3), xs.skip(2).head());
-        // assert_eq!(None, xs.skip(3).head());
-        //
+        assert_eq!(Some(&3), xs.skip(0).head());
+        assert_eq!(Some(&2), xs.skip(1).head());
+        assert_eq!(Some(&1), xs.skip(1).tail().head());
+        assert_eq!(Some(&1), xs.skip(2).head());
+        assert_eq!(None, xs.skip(3).head());
+
         // assert_eq!(None, xs.take(0).head());
         // assert_eq!(Some(&3), xs.take(1).head());
         // assert_eq!(None, xs.take(1).tail().head());
