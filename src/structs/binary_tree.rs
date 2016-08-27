486e74b45370f55de66054413bf5342f9d41edd9
Alexander Lopatin <alopatindev@gmail.com>
2016-08-27 05:50:39 +0300

    added binary_tree

---
+++ src/structs/binary_tree.rs
@@ -0,0 +1,93 @@
+use std::rc::Rc;
+
+pub struct BinaryTree<T: Clone + PartialEq> {
+    root: Link<T>,
+}
+
+pub type Link<T> = Option<Rc<Node<T>>>;
+
+pub struct Node<T: Clone + PartialEq> {
+    data: T,
+    left: Link<T>,
+    right: Link<T>,
+}
+
+impl<T: Clone + PartialEq> BinaryTree<T> {
+    pub fn new(data: T, left: BinaryTree<T>, right: BinaryTree<T>) -> Self {
+        let rc_node = Rc::new(Node {
+            data: data,
+            left: left.root,
+            right: right.root,
+        });
+
+        BinaryTree { root: Some(rc_node) }
+    }
+
+    pub fn new_leaf(data: T) -> Self {
+        let rc_node = Rc::new(Node {
+            data: data,
+            left: None,
+            right: None,
+        });
+
+        BinaryTree { root: Some(rc_node) }
+    }
+
+    pub fn from_node(node: &Link<T>) -> BinaryTree<T> {
+        BinaryTree { root: node.clone() }
+    }
+
+    pub fn new_empty() -> Self {
+        BinaryTree { root: None }
+    }
+
+    pub fn data(&self) -> Option<&T> {
+        self.root.as_ref().map(|rc_node| &rc_node.data)
+    }
+
+    pub fn left_data(&self) -> Option<&T> {
+        self.root.as_ref().map_or(None, |rc_node| {
+            rc_node.left.as_ref().map(|rc_left_node| &rc_left_node.data)
+        })
+    }
+
+    pub fn right_data(&self) -> Option<&T> {
+        self.root.as_ref().map_or(None, |rc_node| {
+            rc_node.right.as_ref().map(|rc_right_node| &rc_right_node.data)
+        })
+    }
+
+    pub fn left(&self) -> BinaryTree<T> {
+        self.root
+            .as_ref()
+            .map_or(BinaryTree::new_empty(),
+                    |rc_node| BinaryTree::from_node(&rc_node.as_ref().left))
+    }
+
+    pub fn right(&self) -> BinaryTree<T> {
+        self.root
+            .as_ref()
+            .map_or(BinaryTree::new_empty(),
+                    |rc_node| BinaryTree::from_node(&rc_node.as_ref().right))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        let empty = BinaryTree::new_empty();
+        let left = BinaryTree::new_leaf(0);
+        let right_right = BinaryTree::new_leaf(3);
+        let right = BinaryTree::new(2, empty, right_right);
+        let root = BinaryTree::new(1, left, right);
+
+        assert_eq!(Some(&1), root.data());
+        assert_eq!(Some(&0), root.left_data());
+        assert_eq!(Some(&2), root.right_data());
+        assert_eq!(None, root.right().left_data());
+        assert_eq!(Some(&3), root.right().right_data());
+    }
+}
