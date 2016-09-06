9128753c73985e7861a0cbdb7b2eac8970e66e7d
Alexander Lopatin <alopatindev@gmail.com>
2016-09-06 05:25:44 +0300

    BitWriter: implemented position; refactoring

--- src/encoding/bitwriter.rs
+++ src/encoding/bitwriter.rs
@@ -6,6 +6,7 @@ pub struct BitWriter<W: Write> {
     output: W,
     buffer: [u8; 1],
     position: u8,
+    bytes_written: u64,
 }
 
 impl<W: Write> BitWriter<W> {
@@ -14,6 +15,7 @@ impl<W: Write> BitWriter<W> {
             output: output,
             buffer: [0],
             position: 0,
+            bytes_written: 0,
         }
     }
 
@@ -27,6 +29,7 @@ impl<W: Write> BitWriter<W> {
             self.position = 0;
             try!(self.output.write_all(&self.buffer));
             self.buffer[0] = 0;
+            self.bytes_written += 1;
         } else {
             self.position += 1;
         }
@@ -62,6 +65,10 @@ impl<W: Write> BitWriter<W> {
         &mut self.output
     }
 
+    pub fn position(&self) -> u64 {
+        8 * self.bytes_written + (self.position as u64)
+    }
+
     pub fn flush(&mut self) {
         if self.position != 0 {
             let _ = self.output.write_all(&self.buffer);
@@ -87,53 +94,79 @@ mod tests {
     quickcheck! {
         fn random_bits(xs: Vec<u8>) -> bool {
             let mut writer = new_writer(xs.len());
+            let mut bits_written = 0;
+
             for &i in &xs {
                 for shift in 0..8 {
                     let shifted_one = 1 << shift;
                     let data = (i & shifted_one) > 0;
                     writer.write_bit(data).unwrap();
+                    bits_written += 1;
                 }
             }
+
+            assert_eq!(bits_written, writer.position());
             writer.flush();
+            assert_eq!(bits_written, writer.position());
+
             check_u8_data(&xs[..], &writer)
         }
 
         fn random_bytes(xs: Vec<u8>) -> bool {
+            let mut bits_written = 0;
+
             let mut writer = new_writer(xs.len());
             for &i in &xs {
                 writer.write_u8(i).unwrap();
+                bits_written += 8;
             }
+
+            assert_eq!(bits_written, writer.position());
             writer.flush();
+            assert_eq!(bits_written, writer.position());
+
             check_u8_data(&xs[..], &writer)
         }
 
         fn random_u64s(xs: Vec<u64>) -> bool {
             let mut writer = new_writer(xs.len());
+            let mut bits_written = 0;
+
             for &i in &xs {
                 writer.write_u64(i).unwrap();
+                bits_written += 64;
             }
+
+            assert_eq!(bits_written, writer.position());
             writer.flush();
+            assert_eq!(bits_written, writer.position());
+
             check_u64_data(&xs[..], &writer)
         }
 
         fn random_mixed_types(xs: Vec<u8>) -> bool {
             let mut writer = new_writer(xs.len());
             let mut bytes = true;
+            let mut bits_written = 0;
 
             for &i in &xs {
                 if bytes {
                     writer.write_u8(i).unwrap();
+                    bits_written += 8;
                 } else {
                     for shift in 0..8 {
                         let shifted_one = 1 << shift;
                         let data = (i & shifted_one) > 0;
                         writer.write_bit(data).unwrap();
+                        bits_written += 1;
                     }
                 }
                 bytes = !bytes;
             }
-            writer.flush();
 
+            assert_eq!(bits_written, writer.position());
+            writer.flush();
+            assert_eq!(bits_written, writer.position());
             check_u8_data(&xs[..], &writer)
         }
     }
@@ -141,7 +174,7 @@ mod tests {
     #[test]
     fn bits() {
         let mut writer = new_writer(2);
-        assert_position(0, &writer);
+        assert_bytes_written(0, &writer);
 
         writer.write_bit(true).unwrap();
         writer.write_bit(false).unwrap();
@@ -149,11 +182,11 @@ mod tests {
         writer.write_bit(true).unwrap();
         writer.write_bit(true).unwrap();
         writer.write_bit(true).unwrap();
-        assert_position(0, &writer);
+        assert_bytes_written(0, &writer);
         writer.write_bit(false).unwrap();
-        assert_position(0, &writer);
+        assert_bytes_written(0, &writer);
         writer.write_bit(false).unwrap();
-        assert_position(1, &writer);
+        assert_bytes_written(1, &writer);
         assert_data(&[0b00111001], &writer);
 
         writer.write_bit(true).unwrap();
@@ -163,9 +196,9 @@ mod tests {
         writer.write_bit(false).unwrap();
         writer.write_bit(false).unwrap();
         writer.write_bit(false).unwrap();
-        assert_position(1, &writer);
+        assert_bytes_written(1, &writer);
         writer.write_bit(false).unwrap();
-        assert_position(2, &writer);
+        assert_bytes_written(2, &writer);
 
         assert_data(&[0b00111001, 0b00001111], &writer);
     }
@@ -173,11 +206,11 @@ mod tests {
     #[test]
     fn two_bytes() {
         let mut writer = new_writer(2);
-        assert_position(0, &writer);
+        assert_bytes_written(0, &writer);
         writer.write_u8(12).unwrap();
-        assert_position(1, &writer);
+        assert_bytes_written(1, &writer);
         writer.write_u8(34).unwrap();
-        assert_position(2, &writer);
+        assert_bytes_written(2, &writer);
         assert_data(&[12, 34], &writer);
     }
 
@@ -187,11 +220,11 @@ mod tests {
         writer.write_bit(true).unwrap();
         writer.write_bit(false).unwrap();
         writer.write_bit(true).unwrap();
-        assert_position(0, &writer);
+        assert_bytes_written(0, &writer);
         writer.write_u8(1).unwrap();
-        assert_position(1, &writer);
+        assert_bytes_written(1, &writer);
         writer.write_bit(true).unwrap();
-        assert_position(1, &writer);
+        assert_bytes_written(1, &writer);
         assert_data(&[0b00001101], &writer);
         writer.flush();
         assert_data(&[0b00001101, 0b00001000], &writer);
@@ -217,7 +250,7 @@ mod tests {
         expect == get_u64_data(writer).as_slice()
     }
 
-    fn assert_position(expect: u64, writer: &MockWriter) {
+    fn assert_bytes_written(expect: u64, writer: &MockWriter) {
         assert_eq!(expect, writer.get_ref().position());
     }
 
