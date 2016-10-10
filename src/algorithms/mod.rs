cd0e92827e99cea48f0671d09d9a407a5096ecab
Alexander Lopatin <alopatindev@gmail.com>
2016-10-10 17:05:43 +0300

    quicksort: implement with recursion

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,5 +1,6 @@
 pub mod binary_search;
-pub mod insertion_sort;
 pub mod bubble_sort;
+pub mod insertion_sort;
 pub mod merge_sort;
 pub mod max_subarray;
+pub mod quick_sort;
--- src/algorithms/quick_sort.rs
+++ src/algorithms/quick_sort.rs
@@ -1,7 +1,53 @@
+use std::fmt::Debug;
+
 pub fn quick_sort<T>(a: &mut Vec<T>) -> &mut Vec<T>
-    where T: PartialOrd + Copy
+    where T: PartialOrd + Copy + Debug
 {
-    unimplemented!()
+    let n = a.len();
+    if n > 0 {
+        helper(a, 0, n - 1);
+    }
+    a
+}
+
+pub fn helper<T>(a: &mut Vec<T>, low: usize, high: usize)
+    where T: PartialOrd + Copy + Debug
+{
+    let pivot = {
+        let mut low = low;
+        let mut high = high;
+        let mut pivot = low;
+
+        while low != high {
+            if pivot == low {
+                if a[high] < a[pivot] {
+                    a.swap(high, pivot);
+                    pivot = high;
+                } else {
+                    high -= 1;
+                }
+            } else {
+                if a[low] > a[pivot] {
+                    a.swap(low, pivot);
+                    pivot = low;
+                } else {
+                    low += 1;
+                }
+            }
+        }
+
+        pivot
+    };
+
+    let n = a.len();
+
+    if pivot > 0 && low < pivot - 1 {
+        helper(a, low, pivot - 1);
+    }
+
+    if pivot < n && high > pivot + 1 {
+        helper(a, pivot + 1, high);
+    }
 }
 
 #[cfg(test)]
@@ -21,6 +67,16 @@ mod tests {
             b.sort();
             b.as_slice() == quick_sort(&mut a).as_slice()
         }
+    }
+
+    #[test]
+    fn test() {
+        // example from https://www.youtube.com/watch?v=3OLTJlwyIqQ
+        let mut a = vec![5, 2, 6, 1, 3, 4];
+        let mut b = a.clone();
+        quick_sort(&mut a);
+        b.sort();
+        assert_eq!(b.as_slice(), quick_sort(&mut a).as_slice());
     }
 
     #[bench]
