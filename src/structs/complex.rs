ebb6fa503328362fa8949f3e53d1d4930b60fce7
Alexander Lopatin <alopatindev@gmail.com>
2016-10-10 20:10:03 +0300

    Complex: implement sqrt

--- src/structs/complex.rs
+++ src/structs/complex.rs
@@ -1,4 +1,5 @@
 use std::ops::{Add, Sub, Mul, Div};
+use std::result;
 
 #[derive(Debug, PartialEq)]
 pub struct Complex {
@@ -6,12 +7,34 @@ pub struct Complex {
     imaginary: f64,
 }
 
+pub type Result<'a> = result::Result<Complex, &'a str>;
+
 impl Complex {
     pub fn new(real: f64, imaginary: f64) -> Self {
         Complex {
             real: real,
             imaginary: imaginary,
         }
+    }
+
+    pub fn sqrt(&self) -> Result {
+        // see https://en.wikipedia.org/wiki/Complex_number#Square_root
+
+        if self.imaginary == 0.0 {
+            return Err("Imaginary part is zero");
+        }
+
+        let k = (self.real * self.real + self.imaginary * self.imaginary).sqrt();
+
+        let gamma = ((self.real + k) * 0.5).sqrt();
+
+        let sign = self.imaginary.signum();
+        let delta = sign * ((k - self.real) * 0.5).sqrt();
+
+        Ok(Complex {
+            real: gamma,
+            imaginary: delta,
+        })
     }
 }
 
@@ -64,6 +87,8 @@ impl Div for Complex {
 mod tests {
     use super::*;
 
+    const EPSILON: f64 = 0.0001;
+
     #[test]
     fn add() {
         assert_eq!(Complex::new(12.0, 14.0),
@@ -90,13 +115,30 @@ mod tests {
 
     #[test]
     fn divide() {
-        let epsilon = 0.0001;
-        let result = Complex::new(5.0, 2.0) / Complex::new(7.0, 12.0);
-        assert!((0.305699482 - result.real).abs() < epsilon);
-        assert!((-0.238341969 - result.imaginary).abs() < epsilon);
+        assert_eq_complex(Complex::new(0.305699482, -0.238341969),
+                          Complex::new(5.0, 2.0) / Complex::new(7.0, 12.0));
 
-        let result = Complex::new(5.0, -2.0) / Complex::new(-7.0, 12.0);
-        assert!((-0.305699482 - result.real).abs() < epsilon);
-        assert!((-0.238341969 - result.imaginary).abs() < epsilon);
+        assert_eq_complex(Complex::new(-0.305699482, -0.238341969),
+                          Complex::new(5.0, -2.0) / Complex::new(-7.0, 12.0));
+    }
+
+    #[test]
+    fn sqrt() {
+        assert_eq_complex_results(Err("Imaginary part is zero"), Complex::new(1.0, 0.0).sqrt());
+        assert_eq_complex_results(Ok(Complex::new(2.27872385, -0.438842117)),
+                                  Complex::new(5.0, -2.0).sqrt());
+        assert_eq_complex_results(Ok(Complex::new(0.438842117, 2.27872385)),
+                                  Complex::new(-5.0, 2.0).sqrt());
+    }
+
+    fn assert_eq_complex(expect: Complex, value: Complex) {
+        assert!((expect.real - value.real) < EPSILON);
+        assert!((expect.imaginary - value.imaginary) < EPSILON);
+    }
+
+    fn assert_eq_complex_results(expect: Result, value: Result) {
+        if expect.is_ok() {
+            assert_eq_complex(expect.unwrap(), value.unwrap());
+        }
     }
 }
