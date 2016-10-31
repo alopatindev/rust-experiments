2aa6a475f00c614956789863415038c93df69e6a
Alexander Lopatin <alopatindev@gmail.com>
2016-11-01 02:04:37 +0300

    substring_match: fix UTF-8 issues

--- src/algorithms/substring_match.rs
+++ src/algorithms/substring_match.rs
@@ -2,8 +2,8 @@ use std::collections::VecDeque;
 
 // O(|text| * |pattern|)
 pub fn slow_substring_find(text: &str, pattern: &str) -> Option<usize> {
-    let n = text.len();
-    let m = pattern.len();
+    let n = text.chars().count();
+    let m = pattern.chars().count();
 
     if n >= m {
         for i in 0..(n - m + 1) {
@@ -20,8 +20,8 @@ pub fn slow_substring_find(text: &str, pattern: &str) -> Option<usize> {
 // https://www.youtube.com/watch?v=H4VrKHVG5qI
 // https://www.youtube.com/watch?v=BRO7mVIFt08&t=36m04s
 pub fn karp_rabin_substring_find(text: &str, pattern: &str) -> Option<usize> {
-    let n = text.len();
-    let m = pattern.len();
+    let n = text.chars().count();
+    let m = pattern.chars().count();
 
     if m == 0 {
         return Some(0);
@@ -31,16 +31,18 @@ pub fn karp_rabin_substring_find(text: &str, pattern: &str) -> Option<usize> {
 
         let pattern_hasher = RollingHash::from(pattern);
 
-        for (i, ch) in text.char_indices() {
+        for (i, ch) in text.chars().enumerate() {
             if text_hasher.len() == m {
                 text_hasher.pop();
             }
 
             text_hasher.append(ch);
 
-            if text_hasher.len() == m && text_hasher.hash() == pattern_hasher.hash() &&
-               string_find(text, pattern, i + 1 - m) {
-                return Some(i + 1 - m); // FIXME
+            if text_hasher.len() == m && text_hasher.hash() == pattern_hasher.hash() {
+                let from = i + 1 - m;
+                if string_find(text, pattern, from) {
+                    return Some(from);
+                }
             }
         }
     }
@@ -49,8 +51,12 @@ pub fn karp_rabin_substring_find(text: &str, pattern: &str) -> Option<usize> {
 }
 
 fn string_find(text: &str, pattern: &str, from: usize) -> bool {
-    let m = pattern.len();
-    &text[from..(from + m)] == pattern
+    let m = pattern.chars().count();
+
+    // basically means &text[from..(from + m)] == pattern
+    let ts: Vec<char> = text.chars().skip(from).take(m).collect();
+    let ps: Vec<char> = pattern.chars().collect();
+    ts == ps
 }
 
 struct RollingHash {
@@ -70,7 +76,7 @@ impl RollingHash {
 
     fn from(text: &str) -> Self {
         let mut result = Self::new();
-        result.reserve(text.len());
+        result.reserve(text.chars().count());
 
         for t in text.chars() {
             result.append(t);
@@ -141,5 +147,8 @@ mod tests {
         assert_eq!(Some(0), substring_find("foo", ""));
         assert_eq!(Some(25),
                    substring_find("zzzzzzzzzzzzz hello world zzzzzzz", " zzzzzzz"));
+        assert_eq!(Some(6),
+                   substring_find("Абвгд Привет Раст еёжз",
+                                  "Привет Раст"));
     }
 }
